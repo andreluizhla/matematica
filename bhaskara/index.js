b9e6202c7522e6c473d98c47be8240e022518222
André Luiz <126507217+andreluizhla@users.noreply.github.com>
2023-11-28 17:46:43 -0300

    modificaçoes

--- bhaskara/index.js
+++ bhaskara/index.js
@@ -5,20 +5,25 @@ var delta = 0
 var x1 = 0
 var x2 = 0
 var res = document.getElementById('res')
+var equacaoEscrita = document.getElementById('equacao')
+var alerta = document.getElementById('alerta')
 a.focus()
+
+function verificarA (a){
+    if (a.value == 0 || a.value.length == 0){
+        res.innerHTML = ''
+        a.value = 1
+        console.error('Ajustado o valor de A para: 1')
+        return alerta.innerHTML += '<p>O valor de A foi ajustado automaticamente para 1, pois A tem que ser diferente de 0. <br>Explicação: Quando o valor de A for igual a 0: 0 vezes x² resultaria em 0 não importa o valor de x, sendo então uma Equação de Primeiro Grau.</p>'
+    } 
+}
 
 function eventA(event) {
     if (event.key === 'Enter'){
         if (a.value.length == 0){
             a.value = 1
         }
-        if (a.value == 0){
-            res.innerHTML = ''
-            a.value = 1
-            console.error('Ajustado o valor de A para: 1')
-            res.innerHTML += '<p>O valor de A foi ajustado automaticamente para 1, pois A tem que ser diferente de 0</p>'
-            res.innerHTML += '<p>Explicação: Quando o valor de A for igual a 0: 0 vezes x² resultaria em 0 não importa o valor de x, sendo então uma Equação de Primeiro Grau.</p>'
-        } 
+        verificarA(a)
         b.focus()
     }
 }
@@ -28,13 +33,7 @@ function eventB(event) {
         if (b.value.length == 0){
             b.value = 0
         }
-        if (a.value == 0){
-            res.innerHTML = ''
-            a.value = 1
-            console.error('Ajustado o valor de A para: 1')
-            res.innerHTML += '<p>O valor de A foi ajustado automaticamente para 1, pois A tem que ser diferente de 0</p>'
-            res.innerHTML += '<p>Explicação: Quando o valor de A for igual a 0: 0 vezes x² resultaria em 0 não importa o valor de x, sendo então uma Equação de Primeiro Grau.</p>'
-        } 
+        verificarA(a)
         c.focus()
     }
 }
@@ -44,62 +43,57 @@ function eventC(event){
         if (c.value.length == 0){
             c.value = 0
         }
-        if (a.value == 0){
-            res.innerHTML = ''
-            a.value = 1
-            console.error('Ajustado o valor de A para: 1')
-            res.innerHTML += '<p>O valor de A foi ajustado automaticamente para 1, pois A tem que ser diferente de 0</p>'
-            res.innerHTML += '<p>Explicação: Quando o valor de A for igual a 0: 0 vezes x² resultaria em 0 não importa o valor de x, sendo então uma Equação de Primeiro Grau.</p>'
-        } 
+        verificarA(a)
         verificar()
         a.focus()
     }
 }
 
 function equacao(a, b, c){
+    equacaoEscrita.innerHTML = ''
+    res.innerHTML += `<p>O valor de <strong>A</strong> é <strong>${a.value}</strong></p>`
     
     if (a.value == 1){
-        res.innerHTML += `A equação é <strong>x²</strong>`
+        equacaoEscrita.innerHTML += `A equação é <strong>x²</strong>`
 
     } else if(a.value == -1){
-        res.innerHTML += `A equação é <strong>- x²</strong>`
+        equacaoEscrita.innerHTML += `A equação é <strong>- x²</strong>`
 
     } else {
-        res.innerHTML += `A equação é <strong>${a.value}x²</strong>`
+        equacaoEscrita.innerHTML += `A equação é <strong>${a.value}x²</strong>`
 
     } if(b.value == 1){
-        res.innerHTML += `<strong> + x</strong>`
+        equacaoEscrita.innerHTML += `<strong> + x</strong>`
 
     } else if(b.value == -1){
-        res.innerHTML += `<strong> - x</strong>`
+        equacaoEscrita.innerHTML += `<strong> - x</strong>`
 
     } else if (b.value > 0){
         //Coloca na equação ax² + bx
-        res.innerHTML += `<strong> + ${b.value}x</strong>`
+        equacaoEscrita.innerHTML += `<strong> + ${b.value}x</strong>`
 
     } else if (b.value < 0){
         //Coloca na equação ax² - bx
-        res.innerHTML += `<strong>${b.value}x</strong>`
+        equacaoEscrita.innerHTML += `<strong>${b.value}x</strong>`
 
     } else if (b.value == 0){
         //Se caso b = 0 então ficaria: ax² +- c
-        res.innerHTML += ''
+        equacaoEscrita.innerHTML += ''
     } if (c.value > 0) {
         //Coloca na equação + c
-        res.innerHTML += `<strong> + ${c.value}</strong>`
+        equacaoEscrita.innerHTML += `<strong> + ${c.value}</strong>`
 
     } else if(c.value < 0){
         //Coloca na equação -c
-        res.innerHTML += `<strong> ${c.value}</strong>`
+        equacaoEscrita.innerHTML += `<strong> ${c.value}</strong>`
 
     } else if(c.value == 0){
-        res.innerHTML += ''
+        equacaoEscrita.innerHTML += ''
 
     }
     
-    res.innerHTML += '<strong> = 0</strong>'  
+    equacaoEscrita.innerHTML += '<strong> = 0</strong>'  
 
-    res.innerHTML += `<p>O valor de <strong>A</strong> é <strong>${a.value}</strong></p>`
     
     if (b.value != 0){
         res.innerHTML += `<p>O valor de <strong>B</strong> é <strong>${b.value}</strong></p>`
@@ -121,18 +115,9 @@ function contas(a, b, c){
 }
 
 function verificar(){
+    verificarA(a)
 
-    if (a.value == 0){
-        res.innerHTML = ''
-        
-        console.error('Ajustado o valor de A para: 1')
-
-        res.innerHTML += '<p>O valor de A foi ajustado automaticamente para 1, pois A tem que ser diferente de 0</p>'
-        res.innerHTML += '<p>Explicação: Quando o valor de A for igual a 0: 0 vezes x² resultaria em 0 não importa o valor de x, sendo então uma Equação de Primeiro Grau.</p>'
-
-        a.value = 1
-    } else {
-
+    if (a.value !== 0 || a.value.length != 0){
         res.innerHTML = ''
 
         contas(a, b, c)
@@ -144,19 +129,11 @@ function verificar(){
         if (delta < 0){
             res.innerHTML += `<p>A equação não possui raiz real, pois Delta é menor que 0</p>`
         } else if (delta > 0){
-
-            if ((x1.toString().split('.')[1] || '').length >= 4){
-                var comprimento = 4
-
-            } else{
-                var comprimento = 0
-
-            }
             res.innerHTML += `<p>A equação possui <strong>2 soluções reais</strong>, sendo elas: </p>`
 
-            res.innerHTML += `<p>O valor de <strong>x'</strong> que é igual a <strong>${x1.toFixed(comprimento)}</strong></p>`
+            res.innerHTML += `<p>O valor de <strong>x'</strong> que é igual a <strong>${x1}</strong></p>`
 
-            res.innerHTML += `<p>O valor de <strong>x"</strong> que é igual a <strong>${x2.toFixed(comprimento)}</strong></p>`
+            res.innerHTML += `<p>O valor de <strong>x"</strong> que é igual a <strong>${x2}</strong></p>`
         } else {
             res.innerHTML += `<p>A equação possui somente <strong>1 solução real</strong>, pois Delta é igual a 0</p>`
 
